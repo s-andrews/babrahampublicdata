19e654f42c62e39da6085243ecdd6ad25414d016
Simon Andrews <simon@proteo.me.uk>
2023-05-17 16:14:41 +0100

    Removed buffering for updates

--- update_data.sh
+++ update_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-python3 sequencing.py > update.log
-python3 pride.py >> update.log
-python3 create_json >> update.log
+python3 -u sequencing.py > update.log
+python3 -u pride.py >> update.log
+python3 -u create_json.py >> update.log
